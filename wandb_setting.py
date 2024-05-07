b6c5039cb2cd5bbfc7b846a54ae9d3d709814a68
MoriyamaK <scotify@akane.waseda.jp>
2024-05-07 16:29:51 +0900

    edit comments

--- wandb_setting.py
+++ wandb_setting.py
@@ -6,7 +6,7 @@ wandb_dir = f'/tmp/wandb_{getpass.getuser()}'
 os.makedirs(wandb_dir, exist_ok=True)
 
 settings = {
-    'WANDB_ENTITY': 'acc_name',    # replace this with your WANDB account name
+    'WANDB_ENTITY': 'account_name',    # replace this with your WANDB account name
     'WANDB_DIR': wandb_dir,
     'WANDB_PROJECT': 'project_name',  # you can change this to the name you like 
     'WANDB_API_KEY': 'wandb_api_key',# replace this with your WANDB API KEY
